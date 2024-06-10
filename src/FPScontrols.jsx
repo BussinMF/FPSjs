0b027a99ee15167f0d9037e374ab9bc9a2afdb66
BussinMF <133649620+BussinMF@users.noreply.github.com>
2024-06-10 13:58:17 +0200

    Update FPScontrols.jsx

--- src/FPScontrols.jsx
+++ src/FPScontrols.jsx
@@ -4,61 +4,29 @@ import { useEffect, useRef, useState } from "react"
 import * as THREE from "three"
 import { useFrame, useThree } from "@react-three/fiber"
 
+const SPEED = 5
+const velocity = new THREE.Vector3()
+const forwardDirectionVector = new THREE.Vector3()
+const sidewaysDirectionVector = new THREE.Vector3()
+
 export default function FPSControls() {
-    const SPEED = 5 // Adjust speed as necessary
     const { camera } = useThree()
     const { rapier, world } = useRapier()
-
     const rigidBodyRef = useRef()
-    const velocity = new THREE.Vector3()
-    const forwardDirectionVector = new THREE.Vector3()
-    const sidewaysDirectionVector = new THREE.Vector3()
-
-    const [subscribeKeys, getKeys] = useKeyboardControls()
     const [isGrounded, setIsGrounded] = useState(true)
-
-    // useEffect(() => {
-    //     const unsubscribeJump = subscribeKeys(
-    //         (state) => state.jump,
-    //         (value) => {
-    //             if (value && isGrounded) {
-    //                 rigidBodyRef.current.applyImpulse({ x: 0, y: 5, z: 0 }) // Adjust the y impulse as necessary
-    //                 setIsGrounded(false)
-    //             }
-    //         }
-    //     )
-    //     return unsubscribeJump
-    // }, [subscribeKeys, isGrounded])
+    const [subscribeKeys, getKeys] = useKeyboardControls()
 
     useFrame((state, delta) => {
         const { forward, backward, leftward, rightward, jump } = getKeys()
-
-        // Wake up rigid body if keys are pressed
+        
         if (forward || backward || leftward || rightward || jump) {
             rigidBodyRef.current.wakeUp()
         }
 
         if (rigidBodyRef.current) {
-            // Check if the character is grounded
-            const origin = rigidBodyRef.current.translation()
-            origin.y -= 0.6
-            const direction = { x: 0, y: -1, z: 0 }
-            const ray = new rapier.Ray(origin, direction)
-            const hit = world.castRay(ray, 10, true)
-
-            if(jump && isGrounded && hit.toi < 0.15)
-            {
-                rigidBodyRef.current.applyImpulse({ x: 0, y: 0.2, z: 0 })
-                setIsGrounded(false)
-            }
-            else if(hit.toi === 0)
-            {
-                setIsGrounded(true)
-            }
-            
-            console.log('Grounded:', isGrounded)
-            console.log(hit.toi);
-
+            /**
+             * Camera
+             */
             let previousCameraYaw = 0
             let accumulatedYaw = 0
             const pos = rigidBodyRef.current.translation()
@@ -72,7 +40,10 @@ export default function FPSControls() {
             previousCameraYaw = currentCameraYaw
             const accumulatedYawQuaternion = new THREE.Quaternion().setFromEuler(new THREE.Euler(0, accumulatedYaw, 0, 'YXZ'))
             rigidBodyRef.current.setRotation(accumulatedYawQuaternion)
-            
+
+            /**
+             * Movement
+             */
             forwardDirectionVector.set(0, 0, -forward + backward)
             sidewaysDirectionVector.set(rightward - leftward, 0, 0)
 
@@ -85,9 +56,28 @@ export default function FPSControls() {
 
             rigidBodyRef.current.setLinvel({
                 x: velocity.x,
-                y: rigidBodyRef.current.linvel().y, // Keep the current y velocity for jumping
+                y: rigidBodyRef.current.linvel().y,
                 z: velocity.z,
             })
+
+            /**
+             * Jump
+             */
+            const origin = rigidBodyRef.current.translation()
+            origin.y -= 0.6
+            const direction = { x: 0, y: -1, z: 0 }
+            const ray = new rapier.Ray(origin, direction)
+            const hit = world.castRay(ray, 10, true)
+
+            if(jump && isGrounded && hit.toi < 0.15)
+            {
+                rigidBodyRef.current.setLinvel({ x: 0, y: 3, z: 0 })
+                setIsGrounded(false)
+            }
+            else if(hit.toi === 0)
+            {
+                setIsGrounded(true)
+            }
         }
     })
 
@@ -95,11 +85,11 @@ export default function FPSControls() {
         <RigidBody
             ref={rigidBodyRef}
             colliders={false}
-            mass={1} // Adjust the mass as necessary
+            mass={1}
             friction={0}
             restitution={0}
             position={[0, 0.6, 0]}
-            enabledRotations={[false, false, false]} // Prevent from falling sideways
+            enabledRotations={[false, false, false]}
         >
             <CapsuleCollider args={[0.3, 0.25]} />
         </RigidBody>
