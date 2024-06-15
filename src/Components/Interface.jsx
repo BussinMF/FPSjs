8eb0b2b97655cdf1a57c5b9c4b25be78aeb2824a
BussinMF <133649620+BussinMF@users.noreply.github.com>
2024-06-15 17:43:02 +0200

    Update Interface.jsx

--- src/Components/Interface.jsx
+++ src/Components/Interface.jsx
@@ -6,7 +6,7 @@ export default function Interface() {
           <p>WASD / ZQSD / arrow keys - Move around </p>
           <p>Space - Jump ( Hold to BunnyHop )</p>
           <p>Shift - Slide</p>
-          <p>Q or A - Dash</p>
+          <p>Q(ENG) or A(FRA) - Dash</p>
           <p>LeftClick - Shoot</p>
         </div>
       </div>
